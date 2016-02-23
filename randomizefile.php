8ebc584a04feb8433f0282060977a57d88b69f6a
Kspal <unconfigured@null.spigotmc.org>
2016-02-23 22:36:58 +0100

    generalise

--- randomizefile.php
+++ randomizefile.php
@@ -9,26 +9,56 @@ function trouveNomUnique($dirPath) {
     return $uniqueval;
 }
 
-$repsrc= "C:\\Users\\noodle\\Desktop\\music\\";
-$repdst= "C:\\Users\\noodle\\Desktop\\music\\randomized\\";
+
+
+if (!array_key_exists(1, $argv)) {
+  echo "usage : randomizefile.php repertoire";
+  exit();
+}
+
+$repsrc= $argv[1];
+
+
+if (!is_dir($repsrc)) {
+  echo "Le répertoire ".$repsrc." n'existe pas.";
+  exit();
+}
+
+
+
+
+if (substr($repsrc, -1)!=DIRECTORY_SEPARATOR) {
+  $repsrc=$repsrc.DIRECTORY_SEPARATOR;
+}
+$repsrc=str_replace("\\", "\\\\", $repsrc);
+
+
+//"C:\\Users\\noodle\\Desktop\\music\\";
+$repdst= $repsrc.'randomized'.DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR;
+//$repdst=str_replace("\\", "/", $repdst);
+if (!is_dir($repdst)) {
+  mkdir($repdst);
+}
+
+array_map('unlink', glob($repdst.'*'));
 
 $tabfile=glob($repsrc."*.mp3");
 shuffle($tabfile);
 
-foreach ($tabfile as $filename) {
-
-    $randfile=trouveNomUnique($repdst);
-    
+foreach ($tabfile as $filename) {    
 
     $f=fopen($filename,'rb');
     if ($f) {
-          $data=fread($f,filesize($filename));
+      $data=fread($f,filesize($filename));
       fclose($f);
-  
-     $f = fopen($repdst.$randfile, "wb");
-      fwrite($f,$data,strlen($data));
-      fclose($f);  
-      }
-    echo "$filename (".$randfile.") occupe " . filesize($filename) . "<br/>\n";
+
+      $randfile=trouveNomUnique($repdst);
+
+      $fd = fopen($repdst.$randfile, "wb");
+      fwrite($fd,$data,strlen($data));
+      fclose($fd);  
+      echo $filename,'-->',$randfile, "\n";
+      }   
+    
 }
 ?>
